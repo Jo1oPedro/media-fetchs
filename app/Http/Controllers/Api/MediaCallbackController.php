6519f7da3b2df3741dd9cc104c55d1f836c5eaf8
Jo1o Pedro <joao.pedreira@estudante.ufjf.br>
2026-03-29 21:00:11 -0300

    remove unused import

--- app/Http/Controllers/Api/MediaCallbackController.php
+++ app/Http/Controllers/Api/MediaCallbackController.php
@@ -6,7 +6,6 @@ use App\Events\MediaStatusUpdated;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\MediaController\UpdateMediaStatusFormRequest;
 use App\Models\Media;
-use Illuminate\Support\Facades\Log;
 
 class MediaCallbackController extends Controller
 {
